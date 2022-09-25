569bfcf1f066cd3c1b77efd0558445ab9fd601f9
Daniil <ZDaniil2000@mail.ru>
2022-09-25 19:49:36 +0300

    Finished CRUD
    Finished CRUD | Доделал CRUD (Cоздание, Чтение, Обновление, Удаление)

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ import express from 'express';
 
 import mongoose from 'mongoose';
 
-import { registerValidation, loginValidation } from './validations/validations.js';
+import {
+  registerValidation,
+  loginValidation,
+  postCreateValidation,
+} from './validations/validations.js';
 
 import checkAuth from './utils/checkAuth.js';
 
@@ -23,7 +27,11 @@ app.post('/auth/login', loginValidation, UserController.login);
 app.post('/auth/register', registerValidation, UserController.register);
 app.get('/auth/me', checkAuth, UserController.getMe);
 
-app.get('/post', checkAuth, UserController.getMe);
+app.get('/posts', PostController.getAll);
+app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, PostController.create);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch('/posts/:id', checkAuth, PostController.update);
 
 app.listen(4444, (err) => {
   if (err) {
